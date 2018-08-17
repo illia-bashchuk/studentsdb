fb959c214502df68b3a46221bc890fe0430a6dae
illia-bashchuk <illia-bashchuk@gmail.com>
2018-08-17 17:30:09 +0300

    add group.html

--- students/views.py
+++ students/views.py
@@ -41,7 +41,8 @@ def students_delete(request, sid):
 
     #Views for Groups
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	return render(request, 'students/group.html', {'groups': groups})
+#	return HttpResponse('<h1>Groups Listing</h1>')
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
