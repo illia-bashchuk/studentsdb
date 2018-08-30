b2fe81636214f7335351a3862bbb1b9150af0921
”Illia <ilvasilich@gmail.com>
2018-08-30 21:30:54 +0300

    changing view for students. Geting data from Database

--- students/views/students.py
+++ students/views/students.py
@@ -1,26 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
+
 
 def students_list(request):
-	students = (
-	    {'id': 1,
-	     'first_name': u'Віталій',
-	     'last_name': u'Подоба',
-	     'ticket': 235,
-	     'image': 'img/me.jpeg'},
-	    {'id': 2,
-	     'first_name': u'Корост',
-	     'last_name': u'Андрій',
-	     'ticket': 2123,
-         'image': 'img/podoba3.jpg'},
-	    {'id': 3,
-	     'first_name': u'Притула',
-	     'last_name': u'Тарас',
-	     'ticket': 5332,
-         'image': 'img/piv.png'},
-	)
-	return render(request, 'students/students_list.html', {'students': students})
+    students = Student.objects.all()
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 # Views for Students
