ae144ffa0f1e01a73b96a0697e66d3367cc1663b
Illia Bashchuk <ilvasilich@gmail.com>
2019-03-19 17:22:22 +0200

    add initDateFields function

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -57,9 +57,17 @@ function initGroupSelector() {
     });
 }
 
+function initDateFields() {
+    $('input.dateinput').datetimepicker({
+        'format': 'YYYY-MM-DD'
+    }).on('dp.hide', function (event) {
+        $(this).blur();
+    });
+}
+
 $(document).ready(function () {
     initJournal();
     initGroupSelector();
-   // initDateFields();
+    initDateFields();
     //initEditStudentPage();
 });
